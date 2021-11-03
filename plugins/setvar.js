4bd7776eec1ac48afe1fe05bd202c211627e6717
ajayancr <91776357+ajayancr@users.noreply.github.com>
2021-11-03 07:42:26 +0530

    Update setvar.js

--- plugins/setvar.js
+++ plugins/setvar.js
@@ -10,15 +10,15 @@ Asena.addCommand({pattern: 'setvar', fromMe: false, desc: Lang.WP}, (async (mess
     var r_text = new Array ();
     
     
-    r_text[0] = "https://www.linkpicture.com/q/IMG-20210629-WA0181.jpg";
+    r_text[0] = "https://i.imgur.com/d3ew3PA.jpeg";
     
     
     var i = Math.floor(1*Math.random())
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `⛦━━💘SETVAR💘━━⛦
-𝐇𝐞𝐥𝐥𝐨👋 𝐈 𝐚𝐦 𝐚 Amalser 𝐛𝐨𝐭.
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `⛦━━🧚SETVAR🧚━━⛦
+𝐇𝐞𝐥𝐥𝐨👋 𝐈 𝐚𝐦 𝐚 sᴀʀᴀ 𝐛𝐨𝐭.
   
 *ẉa.me/➳Setvar*
 ■□■□■□■□■□■□■□■□■□■□
@@ -90,17 +90,17 @@ All setvars are
  To get a song when your number is mentioned
  .setvar TAG_REPPLY:your jid 
 
-     *happy using Amalser bot ❣️*
+     *happy using Sara bot ❣️*
 
-*Editing And Devaloper : Amal -Amalser*
+*Editing And Devaloper : Aj cutzz*
 
-*Erorr Fixing : Farhan -Julie Mwol 💌*
+*Erorr Fixing : Ajayan -Sara💖*
 
-*Supporting : Afnan -Pinky 💌*
+*Supporting : Ajayan 💖*
 
-*User Support : Adhithhari ✨️*
+*User Support : Aj cutzz 💖*
 
-      ═════💢💢═════
+     ❄︎═════🧚═════❄︎
 `}) 
 
 }));
