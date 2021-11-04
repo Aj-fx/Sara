b9619089552fe3668e2606c3e1cc6fe2feb6382d
ajayancr <91776357+ajayancr@users.noreply.github.com>
2021-11-04 21:14:51 +0530

    Update farhanamal.js

--- plugins/farhanamal.js
+++ plugins/farhanamal.js
@@ -33,7 +33,7 @@ Neotro.addCommand({pattern: 'help', fromMe: false, dontAddCommandList: true}, (a
        
        const button = {
         buttonText: '𝗖𝗟𝗜𝗖𝗞 𝗛𝗘𝗥𝗘',
-        description:"꧁•««★𝗦𝗔𝗥𝗔 𝗕𝗢𝗧★»»•꧂",
+        description:"꧁•««★««★𝗦𝗔𝗥𝗔_𝗕𝗢𝗧★»»★»»•꧂",
         sections: sections,
         listType: 1
        }
